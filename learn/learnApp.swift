1ceb446d89960c1e910ead07e5b0a9b6ba3919c0
Dostan <turlybekdostan@gmail.com>
2025-03-26 20:23:12 +0500

    Initial Commit

---
+++ learn/learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnApp.swift
+//  learn
+//
+//  Created by Dostan Turlybek on 26.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
